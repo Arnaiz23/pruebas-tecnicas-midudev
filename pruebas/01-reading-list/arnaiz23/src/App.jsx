9120121e5946f3b56f1eb300e157de5dbb39e7ee
Arnaiz23 <aaf110501@gmail.com>
2023-07-19 17:56:58 +0200

    feat: 🎸 Add the styles

--- pruebas/01-reading-list/arnaiz23/src/App.jsx
+++ pruebas/01-reading-list/arnaiz23/src/App.jsx
@@ -12,10 +12,6 @@ function App() {
   const [pages, setPages] = useState(0)
 
   const filterBooks = (booksDb, genre, pages) => {
-    // if (genre === "all") {
-    //   return booksDb.library.map(book => book)
-    // }
-    // return booksDb.library.filter(({ book }) => book.genre === genre && book.pages >= pages)
     return booksDb.library.filter(({ book }) => {
       return book.pages >= pages && (genre === "all" || book.genre === genre)
     })
@@ -24,40 +20,52 @@ function App() {
   const booksList = filterBooks(booksDb, genre, pages)
 
   return (
-    <>
-      <h1 className="text-xl">Libros</h1>
+    <div className="bg-gray-800 text-white h-screen">
+      <header className="w-full p-8 flex flex-col gap-3">
+        {/* TODO: length fake because is the same always */}
+        <h2 className="text-2xl">{booksDb.library.length} libros disponibles</h2>
 
-      <div>
-        <label htmlFor={pagesId}>Filtrar por páginas</label>
-        <input
-          type="range"
-          id={pagesId}
-          min={0}
-          max={1500}
-          value={pages}
-          onChange={(e) => {
-            setPages(e.target.value)
-          }}
-        />
-        <span>{pages}</span>
-      </div>
-      <div>
-        <label htmlFor={genreId}>Filtrar por género</label>
-        <select
-          id={genreId}
-          onChange={(e) => {
-            setGenre(e.target.value)
-          }}
-        >
-          <option value="all">Todos</option>
-          {genresList.map((genre) => (
-            <option value={genre} key={genre}>
-              {genre}
-            </option>
-          ))}
-        </select>
-        <span>{booksList.length}</span>
-      </div>
+        <div className="flex justify-start items-center gap-3">
+          <div className="flex flex-col justify-center items-center gap-3">
+            <label htmlFor={pagesId}>Filtrar por páginas</label>
+            <div className="flex justify-center items-center gap-2">
+              <input
+                type="range"
+                id={pagesId}
+                min={0}
+                max={1500}
+                value={pages}
+                onChange={(e) => {
+                  setPages(e.target.value)
+                }}
+              />
+              <span>{pages}</span>
+            </div>
+          </div>
+
+          <div className="flex flex-col justify-center items-center gap-3">
+            <label htmlFor={genreId}>Filtrar por género</label>
+            <div className="flex justify-center items-center gap-2">
+              <select
+                id={genreId}
+                onChange={(e) => {
+                  setGenre(e.target.value)
+                }}
+                className="bg-transparent border border-white p-2"
+              >
+                <option value="all">Todos</option>
+                {genresList.map((genre) => (
+                  <option value={genre} key={genre}>
+                    {genre}
+                  </option>
+                ))}
+              </select>
+              <span>{booksList.length}</span>
+            </div>
+          </div>
+        </div>
+      </header>
+
       <div className="w-9/12 grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-3 m-auto">
         {booksList.map(({ book }) => (
           <img
@@ -68,7 +76,7 @@ function App() {
           />
         ))}
       </div>
-    </>
+    </div>
   )
 }
 
